ae554a93cc64305feded697efdba4e889186873e
Pavel <60060559+pyrogn@users.noreply.github.com>
2023-10-13 21:24:40 +0300

    Clean code

--- main.py
+++ main.py
@@ -1,70 +1,89 @@
 import re
-from collections import defaultdict
+
+Sentence = str
+Corpus = list[Sentence]
+Word = str
+WordIdx = int
+Vocab = dict[Word, WordIdx]
+WordCount = int
+DocTermMatrix = list[list[WordCount]]
 
 
 class CountVectorizer:
-    """Convert a collection of text documents to a matrix of token counts"""
+    """Convert a collection of text documents to a matrix of token counts
+
+    Attributes:
+        doc_term_matrix (list): Matrix with frequency of terms in sentence
+        vocab (dict): Mapping word to column index in document term matrix
+    """
 
     def __init__(self):
-        self.feature_names = []
-        self.vocab = {}
+        self.doc_term_matrix: DocTermMatrix = []
+        self.vocab: Vocab = {}
 
-    def fit_transform(self, corpus: list[str]) -> list[list[int]]:
-        doc_term_matrix, vocab = self._word_cnt2(corpus)
-        self.vocab = vocab
-        return doc_term_matrix
+    def fit_transform(self, corpus: Corpus) -> DocTermMatrix:
+        """Fit on corpus and return document-term matrix"""
+        self.doc_term_matrix, self.vocab = self._build_doc_term_matrix(corpus)
+        return self.doc_term_matrix
 
-    @staticmethod
-    def _word_cnt(corpus):
-        word_cnt_corpus = []
-        vocab = set()
-        for sentence in corpus:
-            word_cnt_dict = defaultdict(int)
-            for word in re.findall(r"\w{2,}", sentence.lower()):
-                word_cnt_dict[word] += 1
-            word_cnt_corpus.append(dict(word_cnt_dict))
+    def _build_doc_term_matrix(
+        self,
+        corpus: Corpus,
+    ) -> tuple[DocTermMatrix, Vocab]:
+        """Building document-term matrix and corresponding vocabulary
 
-            vocab.update(set(word_cnt_dict.keys()))
+        We build doc-term matrix while reading corpus in one pass,
+        but this leaves matrix rows to have different size,
+        so later we zero-pad this matrix to have length = vocabulary size
 
-        vocab = dict(zip(vocab, range(len(vocab))))
-
-        size = len(vocab)
+        Also we can replace vocab_size with len(word2dict), but this might
+        be less clear than now
+        """
         doc_term_matrix = []
-        for sentence in word_cnt_corpus:
-            ll = [0] * size
-            for word in sentence:
-                ll[vocab[word]] += sentence[word]
-            doc_term_matrix.append(ll)
-
-        return doc_term_matrix, vocab
-
-    @staticmethod
-    def _word_cnt2(corpus):
-        word_cnt_corpus = []
-        vocab = dict()
+        word2idx = dict()
         vocab_size = 0
         for sentence in corpus:
-            doc_term_vec = [0] * len(vocab)
-            word_cnt_dict = defaultdict(int)
+            doc_term_vec = [0] * len(word2idx)
             for word in re.findall(r"\w{2,}", sentence.lower()):
-                if word in vocab:
-                    doc_term_vec[vocab[word]] += 1
-                else:
+                try:
+                    doc_term_vec[word2idx[word]] += 1
+                except KeyError:
                     doc_term_vec.append(1)
-                    vocab[word] = vocab_size
+                    word2idx[word] = vocab_size  # always incremental value
                     vocab_size += 1
-                word_cnt_dict[word] += 1
-            word_cnt_corpus.append(doc_term_vec)
-        for num_sentence in range(len(word_cnt_corpus)):
-            if len(word_cnt_corpus[num_sentence]) == vocab_size:
-                break
-            cur_vec = word_cnt_corpus[num_sentence]
-            cur_len = len(cur_vec)
-            cur_vec.extend([0] * (vocab_size - cur_len))
 
-        return word_cnt_corpus, vocab
+                assert len(doc_term_vec) == len(word2idx) == vocab_size, (
+                    f"desync between {len(doc_term_vec)=}, "
+                    f"{len(word2idx)=} and {vocab_size=}"
+                )
+
+            doc_term_matrix.append(doc_term_vec)
+
+        padded_doc_term_matrix = self._pad_doc_term_matrix(doc_term_matrix, vocab_size)
+
+        return padded_doc_term_matrix, word2idx
+
+    @staticmethod
+    def _pad_doc_term_matrix(
+        doc_term_matrix: DocTermMatrix,
+        vocab_size: int,
+    ) -> DocTermMatrix:
+        """Right pad vectors in matrix with zero up to vocab_size"""
+        for sentence in doc_term_matrix:
+            if len(sentence) == vocab_size:
+                break
+            sentence.extend([0] * (vocab_size - len(sentence)))
+
+        assert set(map(len, doc_term_matrix)) == {
+            vocab_size
+        }, "some vectors are not correctly padded"
+
+        return doc_term_matrix
 
     def get_feature_names(self) -> list[str]:
+        """Get feature names from built vocabulary, ordered"""
+        if not self.vocab:
+            raise RuntimeError("Run fit_transform first")
         return list(self.vocab)
 
     def toarray(self):
