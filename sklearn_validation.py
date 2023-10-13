dc311738b314a8c21c8984e8c9517b4eabcd9305
Pavel <60060559+pyrogn@users.noreply.github.com>
2023-10-13 18:46:44 +0300

    Make nicer validation file

--- sklearn_validation.py
+++ sklearn_validation.py
@@ -1,55 +1,55 @@
+"""Measure speed comparison with CountVectorizer form sklearn
+Corpus - Frankestein (441k characters, 7.6k lines, 3.1k sentences"""
 from sklearn.feature_extraction.text import CountVectorizer
 from main import CountVectorizer as CustomCountVectorizer
 import numpy as np
 import time
 import subprocess
 
-corpus = [
+_simple_corpus = [
     "This is the first document.",
-    "This document is the second document. mr. bobby's hour-second ",
+    "This document is the second document. mr. bobby's second-hand ",
     "And this is the third one. a cat ",
     "Is this the first document?",
 ]
+
 with open("data/frankenstein.txt", "r") as f:
     corpus = f.read().split(".")
-st1 = time.perf_counter()
+
 vectorizer = CountVectorizer()
+
+st1 = time.perf_counter()
 X = vectorizer.fit_transform(corpus)
-vocab = list(vectorizer.get_feature_names_out())
+vocab = vectorizer.get_feature_names_out()
 en1 = time.perf_counter()
-# print(X.toarray())
-# print(vectorizer.get_feature_names_out())
-# print(X.toarray().shape, type(X.toarray()), type(X.toarray()[0]))
-# print(vectorizer.get_feature_names_out().shape)
-st2 = time.perf_counter()
+
+vocab = list(vocab)
+
 vectorizer_custom = CustomCountVectorizer()
+
+st2 = time.perf_counter()
 X_custom = vectorizer_custom.fit_transform(corpus)
-# print(np.array(X_custom).shape)
 vocab_custom = vectorizer_custom.get_feature_names_out()
-# print(len(vectorizer_custom.get_feature_names()))
 en2 = time.perf_counter()
+
 t1 = en1 - st1
 t2 = en2 - st2
 
-# vectorizer2 = CountVectorizer(analyzer='word', ngram_range=(2, 2))
-# X2 = vectorizer2.fit_transform(corpus)
-# vectorizer2.get_feature_names_out()
 
-assert len(vectorizer.get_feature_names_out()) == len(
-    vectorizer_custom.get_feature_names_out()
-), "different len"
+assert len(vocab) == len(
+    vocab_custom
+), f"different len in vocabulary, {len(vocab)} sklearn vs {len(vocab_custom)} custom"
+
 assert not (
-    set(vectorizer.get_feature_names_out())
-    ^ set(vectorizer_custom.get_feature_names_out())
-), "some extra words: "
+    set(vocab) ^ set(vocab_custom)
+), f"some extra words: {set(vocab) ^ set(vocab_custom)}"
 
-X_custom = np.array(X_custom)[:, [vocab_custom.index(word) for word in vocab]]
-assert np.sum(X.toarray() != X_custom) == 0, "matrices are not equal"
-
-
-# https://stackoverflow.com/a/21901260
-def get_git_revision_hash() -> str:
-    return subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii").strip()
+X_np = X.toarray()
+vocab_custom_dict = dict(zip(vocab_custom, range(len(vocab_custom))))
+X_custom_np = np.array(X_custom)[:, [vocab_custom_dict[word] for word in vocab]]
+assert (
+    np.sum(X_np != X_custom_np) == 0
+), "doc-term matrices are not equal, sorry, you're on your own"
 
 
 def get_git_revision_short_hash() -> str:
@@ -61,24 +61,17 @@ def get_git_revision_short_hash() -> str:
 
 
 with open("stats.csv", "r") as f:
-    # print(f.read().strip().split("\n"))
-    ll = f.read().strip().split("\n")[-1]
-    # print("this line:", f.read().strip().split("\n"))
-    git_hash = ll.split(";")[0]
+    last_line = f.read().strip().split("\n")[-1]
+    last_git_hash_stats = last_line.split(";")[0]
+    cur_git_hash = get_git_revision_short_hash()
 
-    if git_hash != get_git_revision_short_hash():
+    if last_git_hash_stats != cur_git_hash:
         with open("stats.csv", "a") as f:
-            print(
-                ";".join(
-                    map(
-                        str,
-                        [
-                            get_git_revision_short_hash(),
-                            round(t1, 4),
-                            round(t2, 4),
-                            round(t2 / t1, 2),
-                        ],
-                    )
-                ),
-                file=f,
-            )
+            values = [
+                cur_git_hash,
+                round(t1, 4),
+                round(t2, 4),
+                round(t2 / t1, 2),
+            ]
+            values_str = map(str, values)
+            print(";".join(values_str), file=f)
