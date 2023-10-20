2904465c710fb82a123ad31a16af1c919f69b947
Pavel <60060559+pyrogn@users.noreply.github.com>
2023-10-20 13:47:07 +0300

    Add tf-idf vectorizer

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import math
 import re
+from typing import Sequence
 
 Sentence = str
 Corpus = list[Sentence]
 Word = str
 WordIdx = int
 Vocab = dict[Word, WordIdx]
-WordCount = int
+WordCount = (
+    int | float
+)  # this is awkward but necessary to not violate LSP in inheritance
 DocTermMatrix = list[list[WordCount]]
 
 
@@ -43,7 +47,7 @@ class CountVectorizer:
         word2idx: Vocab = {}
         vocab_size = 0
         for sentence in corpus:
-            doc_term_vec = [0] * vocab_size
+            doc_term_vec = [0.0] * vocab_size
             for word in re.findall(r"\w{2,}", sentence.lower()):
                 try:
                     doc_term_vec[word2idx[word]] += 1
@@ -91,6 +95,76 @@ class CountVectorizer:
         return self.get_feature_names()
 
 
+TF = float
+TFMatrix = list[list[TF]]
+IDF = float
+IDFVec = list[IDF]
+TFIDFMatrix = list[list[float]]
+
+
+def tf_transform(count_matrix: DocTermMatrix) -> TFMatrix:
+    """Calculate term-frequency matrix from doc-term matrix"""
+    tf_matrix = []
+    for document in count_matrix:
+        all_terms = sum(document)
+        tf_vec = [cnt_term / all_terms for cnt_term in document]
+        tf_matrix.append(tf_vec)
+    return tf_matrix
+
+
+def idf(count_matrix: DocTermMatrix) -> IDFVec:
+    """Calculate inverse document frequency vector from doc-term matrix"""
+    n_docs = len(count_matrix)
+
+    def vec_tf_idf(cnt_word_in_all_docs: Sequence[int], n_docs: int = n_docs) -> IDF:
+        def idf_formula(n_docs, n_docs_w_word) -> IDF:
+            return math.log((n_docs + 1) / (n_docs_w_word + 1)) + 1
+
+        n_docs_w_word = sum(1 for x in cnt_word_in_all_docs if x > 0)
+        return idf_formula(n_docs, n_docs_w_word)
+
+    idf_vec = [vec_tf_idf(doc_vec) for doc_vec in zip(*count_matrix)]
+    return idf_vec
+
+
+class TfIdfTransformer:
+    """Transformer TF-IDF matrix from doc-term matrix"""
+
+    def __init__(self) -> None:
+        self.tf_idf_matrix: TFIDFMatrix = []
+
+    def fit_transform(self, count_matrix: DocTermMatrix) -> TFIDFMatrix:
+        """Build TF-IDF matrix from doc-term matrix"""
+        tf_matrix = tf_transform(count_matrix)
+        idf_vec = idf(count_matrix)
+
+        for idx_doc in range(len(tf_matrix)):
+            tf_matrix[idx_doc] = [  # changes lists in place
+                tf * idf for tf, idf in zip(tf_matrix[idx_doc], idf_vec)
+            ]
+
+        self.tf_idf_matrix = tf_matrix
+        return self.tf_idf_matrix
+
+
+class TfidfVectorizer(CountVectorizer):
+    """Vectorizer corpus of texts to TF-IDF matrix"""
+
+    def __init__(self, transformer: TfIdfTransformer) -> None:
+        self.transformer = transformer
+        super().__init__()
+
+    def fit_transform(self, corpus: Corpus) -> TFIDFMatrix:
+        """Build TF-IDF matrix from corpus of texts"""
+        count_matrix = super().fit_transform(corpus)
+        tf_idf_matrix = self.transformer.fit_transform(count_matrix)
+        return tf_idf_matrix
+
+
+def round_tf_idf_matrix(tf_idf_matrix: TFIDFMatrix, round_digit=3):
+    return [[round(tf_idf, round_digit) for tf_idf in doc] for doc in tf_idf_matrix]
+
+
 if __name__ == "__main__":
     corpus = [
         "Crock Pot Pasta Never boil pasta again",
@@ -100,3 +174,13 @@ if __name__ == "__main__":
     count_matrix = vectorizer.fit_transform(corpus)
     print(vectorizer.get_feature_names())
     print(count_matrix)
+
+    transformer = TfIdfTransformer()
+    tfidf_vectorizer = TfidfVectorizer(transformer)
+    tf_idf_matrix = tfidf_vectorizer.fit_transform(corpus)
+    print(tf_idf_matrix)
+
+    assert round_tf_idf_matrix(tf_idf_matrix) == [
+        [0.201, 0.201, 0.286, 0.201, 0.201, 0.201, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.143, 0.0, 0.0, 0.0, 0.201, 0.201, 0.201, 0.201, 0.201, 0.201],
+    ], "TF-IDF matrix is wrong"
